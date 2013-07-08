4c1219359f3fb62772b0ccf0ff469363d2caef5a
carolinerosa.nave <carolinerosa.nave@1039dd60-a614-ffa4-02b9-4e0b62539762>
2013-07-08 19:17:13 +0000

    Touch funcionando, ao soltar o obj ele volta para a posição inicial

--- trunk/Brinquedo/src/com/example/brinquedo1/CarregarAssets.java
+++ trunk/Brinquedo/src/com/example/brinquedo1/CarregarAssets.java
@@ -18,7 +18,8 @@ public class CarregarAssets {
 	int alt;
 	int X;
 	int Y;
-	Rect[] rect=new Rect[3];
+	Rect[] rect = new Rect[3];
+
 	public CarregarAssets(Context context, Paint paint) {
 
 		// int current = rnd.nextInt(3);
@@ -31,10 +32,10 @@ public class CarregarAssets {
 		geometricFigures[4] = img.ImageManager("macacoCor.png");
 		geometricFigures[5] = img.ImageManager("leaoCor.png");
 		geometricFigures[6] = geometricFigures[3];
-		rect[0]=new Rect();
-		rect[1]=new Rect();
-		rect[2]=new Rect();
-		
+		rect[0] = new Rect();
+		rect[1] = new Rect();
+		rect[2] = new Rect();
+
 		this.paint = paint;
 
 	}
@@ -51,33 +52,61 @@ public class CarregarAssets {
 	public void setconfig(int larg, int Alt) {
 		this.larg = larg;
 		this.alt = Alt;
-		rect[0].set(this.larg/40, this.alt/30, this.larg/6,(int)(1.5f*this.alt/6));
-		rect[1].set(this.larg/40, this.alt/3, this.larg/6,(int)( 3.5f*this.alt/6));
-		rect[2].set(this.larg/40, 2*this.alt/3, this.larg/6, (int)(5.5f*this.alt/6));
+		rect[0].set(this.larg / 40, this.alt / 30, this.larg / 6,
+				(int) (1.5f * this.alt / 6));
+		rect[1].set(this.larg / 40, this.alt / 3, this.larg / 6,
+				(int) (3.5f * this.alt / 6));
+		rect[2].set(this.larg / 40, 2 * this.alt / 3, this.larg / 6,
+				(int) (5.5f * this.alt / 6));
 	}
 
 	public void setXY(int x, int y) {
 		this.X = x;
 		this.Y = y;
 	}
-	public Rect[] getRect(){
+
+	public Rect[] getRect() {
 		return rect;
+	}
+
+	public void setRectInicial(Rect rec) {
+		for (int i = 0; i < rect.length; i++) {
+			if (rect[i] == rec && i!=0) {
+
+				rect[i].set(this.larg / 40, i * this.alt / 3, this.larg / 6,
+						(int) ((i * 2 + 1.5f) * this.alt / 6));
+			}
+			if(rect[i]==rec && i==0){
+				rect[i].set(this.larg / 40, this.alt / 30, this.larg / 6,
+						(int) ((i * 2 + 1.5f) * this.alt / 6));
+				
+			}
+
+		}
+
+	}
+
+	public void setRect(Rect i) {
+
+		i.set((int) (X - i.width() / 2), (int) (Y - i.height() / 2),
+				(int) (X + i.width() / 2), (int) (Y + i.height() / 2));
+
 	}
 
 	public void Draw(Canvas canvas) {
 
-		canvas.drawBitmap(geometricFigures[3],null,rect[0], paint);
-		canvas.drawBitmap(geometricFigures[4],null, rect[1],paint);
+		canvas.drawBitmap(geometricFigures[3], null, rect[0], paint);
+		canvas.drawBitmap(geometricFigures[4], null, rect[1], paint);
 
-		canvas.drawBitmap(geometricFigures[5],null, rect[2],paint);
-	
-		//	canvas.drawBitmap(geometricFigures[0], 0, 0, paint);
-	//	canvas.drawBitmap(geometricFigures[1], larg / 3, 0, paint);
-	//	canvas.drawBitmap(geometricFigures[2], larg - 100, 0, paint);
+		canvas.drawBitmap(geometricFigures[5], null, rect[2], paint);
+
+		// canvas.drawBitmap(geometricFigures[0], 0, 0, paint);
+		// canvas.drawBitmap(geometricFigures[1], larg / 3, 0, paint);
+		// canvas.drawBitmap(geometricFigures[2], larg - 100, 0, paint);
 
 		// Imagem que ficará na parte de baixo (figura geométrica em PB).
-//		if (geometricFigures[6] != null)
-//			canvas.drawBitmap(geometricFigures[6], X, Y, paint);
+		// if (geometricFigures[6] != null)
+		// canvas.drawBitmap(geometricFigures[6], X, Y, paint);
 
 	}
 }
