039a0c908f2ff2f98e78b039625b73aa69647464
carolinerosa.nave <carolinerosa.nave@1039dd60-a614-ffa4-02b9-4e0b62539762>
2013-07-20 06:51:02 +0000

    Animações do menu e tela de seleção de fases.

--- trunk/Brinquedo/src/Gerenciadores/SoundManager.java
+++ trunk/Brinquedo/src/Gerenciadores/SoundManager.java
@@ -9,73 +9,63 @@ import android.util.Log;
 public class SoundManager {
 
 	private String TAG = "Sound Manager";
-	
-	//private AudioManager audioManager;
+
+	// private AudioManager audioManager;
 	private HashMap<String, MediaPlayer> songs;
 	private static SoundManager instance;
-	
+
 	private SoundManager() {
 		this.songs = new HashMap<String, MediaPlayer>();
 	}
-	
-	public static SoundManager getInstance()
-	{
-		if(instance == null)
-		{
+
+	public static SoundManager getInstance() {
+		if (instance == null) {
 			instance = new SoundManager();
 
-			
 		}
-		
+
 		return instance;
 	}
-	
-	public void playSound(final int source, String name,boolean isLooping, Context context)
-	{
+
+	public void playSound(final int source, String name, boolean isLooping,
+			Context context) {
+
+			final MediaPlayer mp = MediaPlayer.create(context, source);
+
+			try {
+				mp.setLooping(isLooping);
+				mp.start();
+				this.songs.put(name, mp);
+			} catch (Exception e) {
+				mp.stop();
+				Log.i(TAG, "Erro no som");
+			}
 		
-		final MediaPlayer mp = MediaPlayer.create(context, source);
-		
-		try{
-			mp.setLooping(isLooping);
-			mp.start();
-			this.songs.put(name, mp);
-		}catch(Exception e)
-		{
-			mp.stop();
-			Log.i(TAG, "Erro no som"); 
+	}
+
+	public void StopSong(String name) {
+		try {
+			this.songs.get(name).stop();
+			this.songs.remove(name);
+			Log.i("SOUNDMANAGER", "STOP");
+		} catch (Exception e) {
+
 		}
 	}
-	
-	public void StopSong(String name)
-	{
-		try{
-		this.songs.get(name).stop();
-		this.songs.remove(name);
-		Log.i("SOUNDMANAGER","STOP");
-		}catch(Exception e)
-		{
-			
-		}
-	}
-	
-	public void StopAllSongs()
-	{
-		for(MediaPlayer currentAudio : songs.values())
-		{
+
+	public void StopAllSongs() {
+		for (MediaPlayer currentAudio : songs.values()) {
 			currentAudio.stop();
 			songs.remove(currentAudio);
 		}
-		
+
 	}
 
-	
-public void PauseSong(String name)
-{
-	try{
-	this.songs.get(name).pause();
-	}catch(Exception e)
-	{
-		
+	public void PauseSong(String name) {
+		try {
+			this.songs.get(name).pause();
+		} catch (Exception e) {
+
+		}
 	}
-}
 }
